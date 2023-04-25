dd170485316e3f4adeb6f5c3945819981db5a454
Bjorn Borud <borud@lab5e.com>
2023-04-25 17:19:21 +0200

    updated to latest (0.2.3)

--- span.rb
+++ span.rb
@@ -1,29 +1,29 @@
 class Span <  Formula
 	desc "Span command line client"
 	homepage "https://github.com/lab5e/spancli"
-	version "0.2.2"
+	version "v0.2.3"
 
 	on_macos do
 		if Hardware::CPU.intel?
-			url "https://github.com/lab5e/spancli/releases/download/v0.2.2/span.amd64-macos.zip"
-			sha256 "9cd05406e4472251f97c045f70c6eaffc12dbef9fc5b588dcbc4014eddf2c370"
+			url "https://github.com/lab5e/spancli/releases/download/v0.2.3/span.amd64-macos.zip"
+			sha256 "9135f33e162214ffabb8a03eeaf79eade2bc78becf2f452e2a2dd7df7237b53b"
 		end
 
 		if Hardware::CPU.arm?
-			url "https://github.com/lab5e/spancli/releases/download/v0.2.2/span.arm64-macos.zip"
-			sha256 "3a66a8de2dd4d48fc4b53fb2920a3c21759140d3266aa6e4f8996df4d3e773b4"
+			url "https://github.com/lab5e/spancli/releases/download/v0.2.3/span.arm64-macos.zip"			
+			sha256 "a4888a23c50e3e09f54f8891ab328b8880ff745b61fa306b2e1381904cf40713"
 		end
 	end
 
 	on_linux do
 		if Hardware::CPU.intel?
-			url "https://github.com/lab5e/spancli/releases/download/v0.2.2/span.amd64-linux.zip"
-			sha256 "99fab8a37d5f5c1da6773e2e1268c0161afd53021d7d999ea1944bbc19ef1cb7"
+			url "https://github.com/lab5e/spancli/releases/download/v0.2.3/span.amd64-linux.zip"
+			sha256 "f100e3ac1fed5d146af8b7c70d1e6f66188ee0f527b9a84731efefd299b4bff6"
 		end
 
 		if Hardware::CPU.arm? && !Hardware::CPU.is_64_bit?
-			url "https://github.com/lab5e/spancli/releases/download/v0.2.2/span.arm5-rpi-linux.zip"
-			sha256 "39be7012d414fabf55cd058f401ef365014a7d6117baa2ad473ffb38d9ecd87a"
+			url "https://github.com/lab5e/spancli/releases/download/v0.2.3/span.arm5-rpi-linux.zip"
+			sha256 "65a2724954158b049ecc17b9142c40c68bd9762653b3d077e89ef1056cd0f6e1"
 		end
 	end
 
